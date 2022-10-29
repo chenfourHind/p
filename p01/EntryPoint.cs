f6393bfb1b7e2fd36189376e170fbc5f3bc9bfee
Hind Chenfour <hind.chenfour123@gmail.com>
2022-10-29 17:27:09 +0000

    First commit

---
+++ p01/EntryPoint.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using p01.Programs;
+
+namespace p01 {
+    class  EntryPoint{
+        public EntryPoint() {
+            exe02();
+        }
+
+        public bool estPremier(int number) {
+            bool validity = true;
+
+            if (number < 0) {
+                return false;
+            }
+
+            for (int i = 2; i < number && validity == true; i++) {
+                if (number % i == 0) {
+                    validity = false;
+                }
+            }
+                return validity;
+        }
+
+        public void exe01() {
+            int number;
+            bool isInteger;
+
+            do {
+                Console.Write("Entrer un nombre premier : ");
+                String value = Console.ReadLine();
+                number = 4;
+                isInteger = int.TryParse(value, out number);
+            } while (!estPremier(number) || isInteger == false);
+
+            Console.Write("Réponse correcte");
+        }
+
+        public void exe02() {
+            int[] tab = { 1, 2, 4, 6, 9, 11, 13 };
+
+            SortedArray array = new SortedArray(tab);
+            array.print();
+            array.insertv2(8);
+            Console.WriteLine("On ajoute " + 8 + " : ");
+            array.print();
+        }
+
+        static void Main(string[] args) {
+            new EntryPoint();
+            Console.Read();
+        }
+    }
+}
---
+++ p01/Programs/SortedArray.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p01.Programs{
+    class SortedArray {
+        private int[] tab;
+        private int length;
+
+        public SortedArray(int[] tab) {
+            this.tab = tab;
+            this.length = tab.Length;
+        }
+
+        public void insert(int number)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (number < tab[i])
+                {
+                    int[] temp = new int[length + 1];
+                    for (int j = 0; j < i + 1; j++)
+                    {
+                        temp[j] = tab[j];
+                    }
+
+                    temp[i] = number;
+
+                    for (int k = i + 1; k < temp.Length; k++)
+                    {
+                        temp[k] = tab[k - 1];
+                    }
+                    length = temp.Length;
+                    tab = temp;
+                    i = length;
+                }
+            }
+        }
+        public void insertv2(int number) {
+            // la methode Array.Resize crée un nouvel tableau avec la nouvelle dimension, et le reference
+            //avec la reference du tableau d'avant, donc elle l'écrase.
+
+            Array.Resize(ref tab, tab.Length + 1);
+            length = tab.Length;
+
+            for (int i = length - 1; i >= 0; i--) {
+                if (number > tab[i - 1]) {
+                    tab[i] = number;
+                    i = -1;
+                }
+
+                else {
+                    tab[i] = tab[i - 1];
+                }
+            }
+        }
+
+        public void print() {
+            for (int i = 0; i < length; i++) {
+                Console.WriteLine(" - " + tab[i]);
+            }
+        }
+    }
+}
